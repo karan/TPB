fa6b6431767f30c847f98f5c4437cc5f105cf962
Unai Zalakain <unai@gisa-elkartea.org>
2013-12-08 11:30:44 +0100

    Fixed #45 -- added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -17,4 +17,18 @@ setup(
     license='MIT License',
     description='Unofficial Python API for ThePirateBay.',
     long_description='Unofficial Python API for ThePirateBay. Usage: https://github.com/thekarangoel/TPB.',
+    classifiers=[
+        'Development Status :: 3 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Utilities',
+    ],
 )
