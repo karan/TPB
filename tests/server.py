215e6449d926a391919b15fcd9666696989f8a01
Unai Zalakain <unai@gisa-elkartea.org>
2013-12-17 19:15:00 +0100

    Switch to a declarative test server route definition
    Bottle can not fire lambdas as response.

--- tests/server.py
+++ tests/server.py
@@ -2,32 +2,27 @@ from time import sleep
 from os import path
 from multiprocessing import Process
 
-from bottle import Bottle, run, template
+from bottle import Bottle, route, run, template
 
 
 PRESETS_DIR = path.join(path.dirname(__file__), 'presets')
 
 
-class TPBApp(Bottle):
-    presets = {
-            '/search/<query>/<page>/<ordering>/<category>': 'search.html',
-            '/recent/<page>': 'recent.html',
-            '/top/<category>': 'top.html',
-            '/torrent/<id>/<name>/': 'torrent.html',
-            }
+def template_response(func):
+    def wrapper(*args, **kwargs):
+        filename = func(*args, **kwargs)
+        with open(path.join(PRESETS_DIR, filename)) as f:
+            content = f.read()
+        return template(content)
+    return wrapper
 
+
+class TPBApp(Bottle):
     def __init__(self, host='localhost', port=8000):
         super(TPBApp, self).__init__()
         self.host = host
         self.port = port
         self.process = None
-        self.build_routes()
-
-    def build_routes(self):
-        for url, preset in self.presets.items():
-            with open(path.join(PRESETS_DIR, preset)) as preset:
-                content = preset.read()
-            self.route(url)(lambda **kwargs: template(content))
 
     def run(self):
         run(self, host=self.host, port=self.port, debug=False, quiet=True)
@@ -45,6 +40,27 @@ class TPBApp(Bottle):
     def url(self):
         return 'http://{}:{}'.format(self.host, self.port)
 
+tpb = TPBApp()
+
+@tpb.route('/search/<query>/<page>/<ordering>/<category>')
+@template_response
+def search(**kwargs):
+    return 'search.html'
+
+@tpb.route('/recent/<page>')
+@template_response
+def recent(**kwargs):
+    return 'recent.html'
+
+@tpb.route('/top/<category>')
+@template_response
+def top(**kwargs):
+    return 'top.html'
+
+@tpb.route('/torrent/<id>/<name>')
+@template_response
+def torrent(**kwargs):
+    return 'torrent.html'
 
 if __name__ == '__main__':
-    TPBApp('localhost', 8000).run()
+    tpb.run()
